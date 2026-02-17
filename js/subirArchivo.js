13e9dd8941a1aef19c70588634a3b4ef1b87c6c5
Yaelciny <mag07yael@gmail.com>
2026-02-16 23:07:35 -0600

    Se modifico el index haciendolo mas responsivo y agregando los subtemas faltando solo uno de poner, css agregando todo lo del index y js. Se agrego un java Script para el mapa, y imagenes para el index junto con librerias de animacion y del mapa

--- js/subirArchivo.js
+++ js/subirArchivo.js
@@ -10,8 +10,28 @@ foto.addEventListener('change', (e) => {
     if(e.target.files[0]){ //Evita errores si el usuario cancela
         const reader = new FileReader();
         reader.onload = function(e){
-            img.src = e.target.result;
+            const imgBase64 = e.target.result;
+            img.src = imgBase64;
+            
+            analizarImagenOpenIA(imgBase64);
         }
         reader.readAsDataURL(e.target.files[0]); //Convierte el fila a data URL (data 64)
     }
 });
+
+async function analizarImagenOpenIA(imgBase64) { //funcion para comunicarse con Open AI
+    const respuesa = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',  
+        headers: { 'Content-Type': 'application/json',
+                'Authorization': 'Bearer '
+            },
+        body: JSON.stringify({
+            model: "gpt-4o", //Modelo con capacidad de vision
+            messages: [
+                {
+
+                }
+            ]
+        })
+    });
+}
